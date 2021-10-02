8bd8fe8400e49123f068b0948a6ee8a3ed673d12
thatretrodev <thatretrodev@outlook.com>
2021-10-02 11:52:11 -0400

    Use floats instead of integers for OpenGL points

--- src/ggutil.c
+++ src/ggutil.c
@@ -7,9 +7,9 @@ extern "C" {
 	To do it, a Pixel camera is needed to be able to convert the Pixel Coordinates which is done in the form of width and height.
 */
 float[] pixel_coord_to_gl_coord(int x, int y, int camWidth, int camHeight) {
-	int glX = (((x - 0) * (1 - -1)) / (camWidth - 0)) + -1;
-	int glY = (((y - 0) * (1 - -1)) / (camHeight - 0)) + -1;
-	int output[2] = {glX, glY};
+	float glX = (((x - 0) * (1 - -1)) / (camWidth - 0)) + -1;
+	float glY = (((y - 0) * (1 - -1)) / (camHeight - 0)) + -1;
+	float output[2] = {glX, glY};
 	return output;
 }
 
